f524c0da8e4ff3d48474bb95a2970a6c56bbd587
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-04 14:44:04 -0500

    added more gh-pages files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,11 @@ module.exports = function (grunt) {
       src: [
         'readme.md',
         'index.html',
+        'todomvc.html',
         'bower_components/es5-shim/es5-shim.js',
-        'todomvc.html'
+        'bower_components/iframe-api/dist/iframe-api.js',
+        'bower_components/todomvc-app-css/*',
+        'bower_components/todomvc-common/*'
       ]
     }
   });
